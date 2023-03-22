36f641a7eeb2fdaf4c5f212cddb95eba544a9c3a
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-22 11:10:15 +0100

    Added date data successfully

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from database import create_tables
 from crawler import get_event_data
 from crawler.crawler import Crawler
-from database.models import add_locations, add_events
+from database.models import add_locations, add_events, add_works, add_dates
 
 if __name__ == '__main__':
-    create_tables()
-
     crawler = Crawler()
     events_soup = crawler.get_soup(url="https://www.lucernefestival.ch/en/program/summer-festival-23")
     links = crawler.get_links(events_soup, tag_="div", class_="cell shrink show-for-large")
     #
     data = [get_event_data(links[i], crawler) for i in range(0, len(links)) if i == 0][0]
-    # #
-    # # Adding Event Location to the database
-    add_locations(data)
-    add_events(data)
+
+    # create_tables()
+    # Adding Event Location to the database
+    # add_locations(data)
+    # add_events(data)
+    # add_works(data)
+    add_dates(data)
+
+#

f742d47488b4c618689a2f98f649bf9e32566a8b
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-22 14:22:39 +0100

    Almost done, next step is to refactor the code to run this in a loop and make sure it does not break while adding all the date into the database.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from database import create_tables
 from crawler import get_event_data
 from crawler.crawler import Crawler
@@ -8,17 +10,18 @@ if __name__ == '__main__':
     crawler = Crawler()
     events_soup = crawler.get_soup(url="https://www.lucernefestival.ch/en/program/summer-festival-23")
     links = crawler.get_links(events_soup, tag_="div", class_="cell shrink show-for-large")
-    #
+
     data = [get_event_data(links[i], crawler) for i in range(0, len(links)) if i == 0][0]
 
-    # create_tables()
-    # # Adding Event Location to the database
-    # add_locations(data)
-    # add_events(data)
-    # add_works(data)
-    # add_dates(data)
-    # add_date_events_table(data)
-    # add_artists(data)
-    # add_event_artists_table(data)
-    # add_tickets(data)
+    create_tables()
+    # Adding Event Location to the database
+    add_locations(data)
+    add_events(data)
+    add_works(data)
+    add_dates(data)
+    add_date_events_table(data)
+    add_artists(data)
+    add_event_artists_table(data)
+    add_tickets(data)
     add_event_tickets_table(data)
+

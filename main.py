0a3232c3199207ce9501ef075643bf2a6a4b42b9
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-22 07:59:59 +0100

    Adding event data to events table

--- main.py
+++ main.py
@@ -4,16 +4,14 @@ from crawler.crawler import Crawler
 from database.models import add_locations, add_events
 
 if __name__ == '__main__':
-    # create_tables()
+    create_tables()
 
     crawler = Crawler()
     events_soup = crawler.get_soup(url="https://www.lucernefestival.ch/en/program/summer-festival-23")
     links = crawler.get_links(events_soup, tag_="div", class_="cell shrink show-for-large")
     #
     data = [get_event_data(links[i], crawler) for i in range(0, len(links)) if i == 0][0]
-    #
-    # print(data)
-    # Adding Event Location to the database
-    # add_locations(data)
-
-    add_events(data, data['link'])
+    # #
+    # # Adding Event Location to the database
+    add_locations(data)
+    add_events(data)

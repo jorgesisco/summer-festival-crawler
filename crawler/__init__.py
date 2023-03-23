2ff4df610523dc6bddf31238b11c364dcff468e2
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-23 12:21:34 +0100

    Added docstrings to the init files

--- crawler/__init__.py
+++ crawler/__init__.py
@@ -2,3 +2,15 @@ from crawler.data_extraction import get_event_data
 from crawler.crawler import Crawler
 
 crawler = Crawler()
+
+"""
+This module initializes the Crawler class, which is used for extracting the data from whe website. 
+
+By initializing Crawler in the __init__.py file, we make it easier to import and use the class 
+from other modules within this package. 
+
+Usage:
+    from crawler import crawler
+
+    links = crawler.get_links()
+"""
--- database/__init__.py
+++ database/__init__.py
@@ -1,3 +1,17 @@
 from database.models import DataBase
 
 db = DataBase(DATABASE_URL="postgresql://admin:admin@db:5432/database")
+
+
+"""
+This module initializes the DataBase class, which is used for creating the database 
+tables and inserting data instances. 
+
+By initializing DataBase in the __init__.py file, we make it easier to import and use the class 
+from other modules within this package. 
+
+Usage:
+    from database import db
+
+    db.create_tables()
+"""

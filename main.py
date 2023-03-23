4971f546489c44c30cf3a023fe3e9ef7a2df7462
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-23 12:37:26 +0100

    Fixed the docstring about get_events_per_day function.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     links = crawler.get_links(events_soup, tag_="div", class_="cell shrink show-for-large")
 
     """
-    Running DataBase to create tables, if they exist already, the code won't run
+    Running create_tables to create tables, if they exist already, the code won't run
     """
     db.create_tables()
 
@@ -19,9 +19,12 @@ if __name__ == '__main__':
     Running function to insert data in the database, the active parameter is for running the function or not, if data is already
     in the database, this can be set to False.
     """
-    insert_data_to_postgres(links=links, crawler=crawler, active=False)
+    insert_data_to_postgres(links=links, crawler=crawler, active=True)
 
-    get_events_per_day(active=False)
+    """
+    To run get_events_per_day method to get the plot, comment lines 10, 11, 16 and 22 and run the main.py file
+    """
+    get_events_per_day(active=True)
 
 
 

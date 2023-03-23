8b89844003f90edd64ed6d189c8e53d27570c130
Jorge Sisco <jorgesisco17@gmail.com>
2023-03-23 11:40:28 +0100

    Added docstrings to main.py

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from database_utils import insert_data_to_postgres
 from plotter.plotter import get_events_per_day
 
 if __name__ == '__main__':
+    """
+    Using Crawler instance get_links to retrieve a list with event links
+    """
     events_soup = crawler.get_soup(url="https://www.lucernefestival.ch/en/program/summer-festival-23")
     links = crawler.get_links(events_soup, tag_="div", class_="cell shrink show-for-large")
 
-    # db.create_tables()
+    """
+    Running DataBase to create tables, if they exist already, the code won't run
+    """
+    db.create_tables()
 
     """
-    Inserts data in the database, the active parameter is for running the function or not, if data is already
-    in place, this can be set to False.
-    The add to db methods, have constrains to avoid adding duplicates, keep in mind some info can be reused such as
-    dates, locations, etc.
+    Running function to insert data in the database, the active parameter is for running the function or not, if data is already
+    in the database, this can be set to False.
     """
     insert_data_to_postgres(links=links, crawler=crawler, active=False)
 
